0f71210b24ff54f3f846f549b55fb5b9b009a5e5
Roman Podgaiski <r.podgaiski@pm.me>
2023-07-18 09:01:17 +0300

    0.4.1 Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@
 __Name__ = 'FreeCAD World'
 __Comment__ = 'Flexible parametric design of domical objects'
 __Author__ = 'ropod7'
-__Version__ = '0.4.0'
+__Version__ = '0.4.1'
 __Status__ = 'alpha'
-__Date__ = '10.07.2023'
+__Date__ = '18.07.2023'
 __License__ = 'GNU General Public License v3.0'
 __Web__ = 'https://github.com/ropod7/FreeCAD_World'
 __Wiki__ = 'https://github.com/ropod7/FreeCAD_World/tree/master/docs'
@@ -36,33 +36,34 @@ __Files__ = 'https://github.com/ropod7/FreeCAD_World'
 ###############################################################################
 
 # Root object params (mm):
-DETAILS  = int(13)  # Detalization of polygon [should be divisible by 4 and min is 4 (otherwise experimental)]
-OR     = float(4000) # Outscribed R of polygon (or inscribed polygon) & carriage on X axis
-H1     = float(2000) # Height of 1st 'floor' & carriage on Z (or 0)
-LONG   = float(2000) # Extension & carriage on Y axis (or 0)
+DETAILS  = int(16)  # Detalization of polygon [should be divisible by 4 and min is 4 (otherwise experimental)]
+OR     = float(3500) # Outscribed R of polygon (or inscribed polygon) & carriage on X axis
+H1     = float(1200) # Height of 1st 'floor' & carriage on Z (or 0)
+LONG   = float(0) # Extension & carriage on Y axis (or 0)
 THORUS = dict( # Proportionally expanded (OR becomes IR or both False is DOME)
-    CORNER = bool(True), # Inner side of thorus. Outside R of dome becomes inside R
-    DISC   = bool(True)  # Outer side of thorus. OR = OR*3
+    CORNER = bool(False), # Inner side of thorus. Outside R of dome becomes inside R
+    DISC   = bool(False)  # Outer side of thorus. OR = OR*3
 ) # template of SpreadSheet system of object:
 ROWS    = int(DETAILS/4) # Number of rows revolving around Y axis (min: 0; max: DETAILS/4)
-COLS    = int(DETAILS)   # Number of cols revolving around Z axis (min: 0; max: DETAILS)
+COLS    = int(2)#DETAILS)   # Number of cols revolving around Z axis (min: 0; max: DETAILS)
 
 # Materials (mm)    [ (!!!) it works just with domeFCMacro.py module (!!!) ]:
-MONO   = float(15)        # Sheet mtl/Stone/concrete system (wall thickness). if MONO > 0: not FRAME
+MONO   = float(200)        # Sheet mtl/Stone/concrete system (wall thickness). if MONO > 0: not FRAME
 FRAME = tuple((50, 200)) # (Width, Height) of wooden bar/pipe/sheet mtl. etc: == [Width < Height]
 
 # Construction (mm) [ (!!!) it works just with houseFCMacro.py module (!!!) ]:
 HOUSE = dict( # THORUS still experimental:
-    FRAME    = tuple((50, 200)),# (Width, Height) of wooden bar/pipe/sheet mtl. etc: == [Width < Height]
+    FRAME    = tuple((150, 200)),# (Width, Height) of wooden bar/pipe/sheet mtl. etc: == [Width < Height]
     INSULANT  = float(150),# Insulant thickness. (!!!) Must be lower than FRAME BAR height (!!!).
-    CONTOUR   = float(50), # Ventilated contour (if 0 Gives + 1-3 mm). (!!!) Make sure differences between H1, LONG layers (!!!).
-    COVER     = float(15), # Sheet material thickness (or 0). (!!!) floating CONTOUR space accuracy +\- 2 mm. (!!!)
+    CONTOUR   = float(60), # Ventilated contour (if 0 Gives + 1-3 mm). (!!!) Make sure differences between H1, LONG layers (!!!).
+    COVER     = float(25), # Sheet material thickness (or 0). (!!!) floating CONTOUR space accuracy +\- 2 mm. (!!!)
     # OUTSCRIBE = bool(True) # in case of needs to outscribe THORUS into DOME building according COVER and CONTOUR. (!!!) NOT YET IMPLEMENTED (!!!).
 )
 
 # Compound options:
 EXTEND   = bool(True)   # Root as open contour system (fill up long, h1, cols, rows)
-COMPOUND = bool(True)   # Simple compounded object (if LONG: COLS <= DETAILS/2)
+COMPOUND = bool(False)   # Simple compounded object (if LONG: COLS <= DETAILS/2)
+SLICE    = bool(True)    # if EXTEND and SLICE are True returns sliced polygonal parts of (!!!) HOUSE (!!!)
 
 # Movement (mm):
 ROTATE = dict( # Rotate COMPOUNDed object
@@ -81,13 +82,15 @@ MOVE = dict(     # Move COMPOUNDed object
 )
 
 # Extra options:
-WIREFRAME  = bool(False)   # To reproduce or hide wireframe
-ROOT       = bool(True)    # To show just wireframe if reproduced
-SOLID      = bool(True)    # Experimental attempt to speedup compound presentation in case of [False]
-# PRODUCTION = bool(False) # Produce Root for production (in case of STL or DXF needs)
-PRINT3D   = tuple((0, 0))  # Scale MONO or FRAME (not HOUSE) to 3D printer bed. if > 0 and COMPOUND: do
-CLEANUP    = bool(True)    # Clean up document before build
-CONFIG     = bool(True)    # Switch off CONFIG to extend macros by python3 code
+WIREFRAME   = bool(False)   # To reproduce or hide wireframe
+ROOT        = bool(True)    # To show just wireframe if reproduced
+SOLID       = bool(True)    # Experimental attempt to speedup compound presentation in case of [False]
+# PRODUCTION = bool(False)  # Produce Root for production (in case of STL or DXF needs)
+PRINT3D     = tuple((0, 0)) # Scale MONO or FRAME (not HOUSE) to 3D printer bed. if > 0 and COMPOUND: do
+# NEW_DOC     = bool(False)    # Build in new document (except CLEANUP)
+CLEANUP     = bool(True)    # Clean up document before build
+GUI_CLEANUP = bool(False)   # if CLEANUP: to do update screen during clean up
+CONFIG      = bool(True)    # Switch off CONFIG to extend macros by python3 code
 
 ###################################################################################
 #######################################################################клинопись###
